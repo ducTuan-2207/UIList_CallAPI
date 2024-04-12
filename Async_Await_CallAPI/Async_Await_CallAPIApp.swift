820a63cffae03761f6d276dc5e781c3530f9e4c1
DucTuan <tuanhypc02@gmail.com>
2024-04-12 23:45:17 +0700

    Initial Commit

---
+++ Async_Await_CallAPI/Async_Await_CallAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_CallAPIApp.swift
+//  Async_Await_CallAPI
+//
+//  Created by macOS on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_CallAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Async_Await_CallAPI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Async_Await_CallAPI
+//
+//  Created by macOS on 12/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
